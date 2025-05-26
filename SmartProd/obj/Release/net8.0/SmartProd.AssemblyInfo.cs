11220fb69465cc4cd54f7e7f1f848634e976eef6
Bressanvt <vitorbressanvieira@outlook.com>
2025-05-26 11:31:48 -0300

    Atualização

--- SmartProd/obj/Release/net8.0/SmartProd.AssemblyInfo.cs
+++ SmartProd/obj/Release/net8.0/SmartProd.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartProd")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad478a883f784b1849bf56d3d5364a07701f7c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartProd")]
